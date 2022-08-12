a5d64568c0a9d9d78fcb1e5d331d2575cc16a02b
Triveni Mahatha <triveni692@users.noreply.github.com>
2022-08-12 10:14:07 +0530

    login feature added (#2)

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(require("./routes/question"));
 app.use(require("./routes/contest"));
 app.use(require("./routes/combat_questions"));
+app.use(require("./routes/login"));
 // get driver connection
 const dbo = require("./db/conn");
 
